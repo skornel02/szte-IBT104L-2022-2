6495459eabba5475f7738575db5f60510dcaab5c
hgergely03 <gergely0703@gmail.com>
2023-06-11 09:52:41 +0200

    Főprogramból nyugta felszabadítás lemaradt (programból való kilépés, 4-es opció esetén)

--- Kaszas/Kaszas.c
+++ Kaszas/Kaszas.c
@@ -163,6 +163,7 @@ int main(int argc, char* argv[]) {
 #ifdef DEBUG
                     printf("DEBUG: EXITING");
 #endif
+                    free_nyugta_members(&nyugta);
                     exit(0);
                 }
             }
